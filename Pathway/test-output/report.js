a1a8ab2731161569f4335ffae28ed0b19eaf46db
saiku <saiku@LAPTOP-4MG5Q5BI>
2020-05-27 17:40:26 +0530

    reports

--- Pathway/test-output/report.js
+++ Pathway/test-output/report.js
@@ -332,3 +332,21 @@ formatter.step({
 formatter.match({
   "location": "Renewalflow.legal_user_login_with_valid_credtinals_for_Renewal_Process()"
 });
+formatter.result({
+  "duration": 14029879400,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"#i0116\"}\n  (Session info: chrome\u003d83.0.4103.61)\nFor documentation on this error, please visit: http://seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.14.0\u0027, revision: \u0027aacccce0\u0027, time: \u00272018-08-02T20:19:58.91Z\u0027\nSystem info: host: \u0027LAPTOP-4MG5Q5BI\u0027, ip: \u0027192.168.0.5\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_191\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 83.0.4103.61, chrome: {chromedriverVersion: 83.0.4103.39 (ccbf011cb2d2b..., userDataDir: C:\\Users\\saiku\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:64647}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 10ed0a63ba257c51292be7197a360d09\n*** Element info: {Using\u003did, value\u003di0116}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:548)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:322)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementById(RemoteWebDriver.java:368)\r\n\tat org.openqa.selenium.By$ById.findElement(By.java:188)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:314)\r\n\tat com.Pathway.Base.Basepage.enterText(Basepage.java:95)\r\n\tat com.Pathway.Webpages.Loginpage.doLogin(Loginpage.java:33)\r\n\tat com.Pathway.Users.LegalUser.loginwithLeaglUser(LegalUser.java:25)\r\n\tat com.Pathway.Stepdefinations.Renewalflow.legal_user_login_with_valid_credtinals_for_Renewal_Process(Renewalflow.java:124)\r\n\tat ✽.When LEGAL user login with valid credtinals for Renewal Process(E:/repositeryforgit/Pathway_Automation/Pathway/src/test/java/com/Pathway/Base/Featurefiles/Renewalflow.feature:42)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "Renewalflow.request_send_to_FCC_with_Legal_user_FOR_Renewal_PROCESS()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "Renewalflow.logout_LEGAL_user_after_succesful_Renewal_Request_Send_to_FCC()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
